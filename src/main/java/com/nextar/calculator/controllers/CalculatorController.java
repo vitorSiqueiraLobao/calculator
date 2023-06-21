c09eb759cf6a265dc11d261186013c4dfcf365a4
vitor lobao <vitorsiqueiralobao@gmail.com>
2023-06-21 18:48:03 -0300

    tests

--- src/main/java/com/nextar/calculator/controllers/CalculatorController.java
+++ src/main/java/com/nextar/calculator/controllers/CalculatorController.java
@@ -5,6 +5,7 @@ import com.nextar.calculator.classes.CalculatorParameters;
 import com.nextar.calculator.classes.CalculatorRequest;
 import com.nextar.calculator.classes.CalculatorResult;
 import com.nextar.calculator.repositories.RequestRepository;
+import com.nextar.calculator.services.EvaluateService;
 import com.nextar.calculator.services.RequestService;
 import com.oracle.truffle.js.runtime.objects.Null;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ class CalculatorController {
 
     @Autowired
     private RequestService requestService;
+
+    @Autowired
+    private EvaluateService evaluateService;
 
     @PostMapping("/calculate")
     @ExceptionHandler(Exception.class)
@@ -35,7 +39,7 @@ class CalculatorController {
                 if (!expression.matches("[\\d+\\-*/().]+")) {
                     throw new IllegalArgumentException("Invalid expression");
                 }
-                Double evaluateResult = evaluate(expression);
+                Double evaluateResult = evaluateService.evaluate(expression);
                 result = new CalculatorResult(evaluateResult);
 
                 CalculatorRequest calcRequest = new CalculatorRequest();
@@ -50,72 +54,11 @@ class CalculatorController {
 
             return ResponseEntity.status(HttpStatus.OK).body(result);
         }catch(Exception e){
-            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new CalculatorError(e.getMessage()));
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new CalculatorError(e.getMessage()));
         }
 
     }
 
-    public double evaluate(String expression){
-        while (expression.contains("(")) {
-            int openParenIndex = expression.lastIndexOf("(");
-            int closeParenIndex = expression.indexOf(")", openParenIndex);
-            String subExpression = expression.substring(openParenIndex + 1, closeParenIndex);
-            double subResult = evaluate(subExpression);
-            expression = expression.replace("(" + subExpression + ")", String.valueOf(subResult));
-        }
 
-        // Realiza multiplicação e divisão
-        while (expression.contains("*") || expression.contains("/")) {
-            String expressionWithoutDecimals = expression.replaceAll("\\.\\d+", "");
-            int multiplyIndex = expressionWithoutDecimals.indexOf("*");
-            int divideIndex = expressionWithoutDecimals.indexOf("/");
-            int operatorIndex;
-            if (multiplyIndex >= 0 && divideIndex >= 0) {
-                operatorIndex = Math.min(multiplyIndex, divideIndex);
-            } else if (multiplyIndex >= 0) {
-                operatorIndex = multiplyIndex;
-            } else {
-                operatorIndex = divideIndex;
-            }
-
-            String[] operands = expression.split("(?<=[-+*/])|(?=[-+*/])");
-            double leftOperand = Double.parseDouble(operands[operatorIndex-1]);
-            double rightOperand = Double.parseDouble(operands[operatorIndex+1]);
-            double result;
-            if (expressionWithoutDecimals.charAt(operatorIndex) == '*') {
-                result = leftOperand * rightOperand;
-            } else {
-                result = leftOperand / rightOperand;
-            }
-            expression = expression.replace(operands[operatorIndex-1] + operands[operatorIndex] + operands[operatorIndex+1], String.valueOf(result));
-        }
-
-        // Realiza adição e subtração
-        double result = 0;
-        while (expression.contains("-")||expression.contains("+")){
-            String expressionWithoutDecimals = expression.replaceAll("\\.\\d+", "");
-            int subIndex = expressionWithoutDecimals.indexOf("-");
-            int addIndex = expressionWithoutDecimals.indexOf("+");
-            int operatorIndex;
-            if (subIndex >= 0 && addIndex >= 0) {
-                operatorIndex = Math.min(subIndex, addIndex);
-            } else if (subIndex >= 0) {
-                operatorIndex = subIndex;
-            } else {
-                operatorIndex = addIndex;
-            }
-            String[] operands = expression.split("(?<=[-+*/])|(?=[-+*/])");
-            double leftOperand = Double.parseDouble(operands[operatorIndex-1]);
-            double rightOperand = Double.parseDouble(operands[operatorIndex+1]);
-            if (expressionWithoutDecimals.charAt(operatorIndex) == '+') {
-                result = leftOperand + rightOperand;
-            } else {
-                result = leftOperand - rightOperand;
-            }
-            expression = expression.replace(operands[operatorIndex-1] + operands[operatorIndex] + operands[operatorIndex+1], String.valueOf(result));
-        }
-
-        return result;
-    }
 
 }
--- src/test/java/com/nextar/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/nextar/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,104 @@
 package com.nextar.calculator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nextar.calculator.classes.CalculatorParameters;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CalculatorApplication.class)
+@AutoConfigureMockMvc
 class CalculatorApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testScenario1() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("(2+2)*3");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+		)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(12.0)));
 	}
+
+	@Test
+	public void testScenario2() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("2.2+2.2");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(4.4)));
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(4.4)));
+	}
+
+	@Test
+	public void testScenario3() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("2.3*2.3+5");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(10.29)));
+	}
+
+	@Test
+	public void testScenario4() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("2.33/3");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(0.78)));
+	}
+
+	@Test
+	public void testScenario5() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("1/0");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
+	}
+
+	@Test
+	public void testScenario6() throws Exception {
+		CalculatorParameters request = new CalculatorParameters("((22)+(63))/2");
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(MockMvcRequestBuilders.post("/calculate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request))
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.resultado",equalTo(11)));
+	}
+
 
 }
